6091aab66d885de45426f7ad7beea5852ad0f407
Gilbert Jung <83274792+Gilbert9172@users.noreply.github.com>
2026-01-28 23:39:08 +0900

    feat : 왼쪽 사이드바 on/off 토글

--- _javascript/modules/layouts/sidebar.js
+++ _javascript/modules/layouts/sidebar.js
@@ -1,7 +1,12 @@
 const ATTR_DISPLAY = 'sidebar-display';
+const ATTR_COLLAPSED = 'sidebar-collapsed';
+const STORAGE_KEY = 'sidebar-collapsed';
+
 const $sidebar = document.getElementById('sidebar');
 const $trigger = document.getElementById('sidebar-trigger');
 const $mask = document.getElementById('mask');
+const $collapseBtn = document.getElementById('sidebar-collapse-btn');
+const $expandBtn = document.getElementById('sidebar-expand-btn');
 
 class SidebarUtil {
   static #isExpanded = false;
@@ -14,6 +19,32 @@ class SidebarUtil {
   }
 }
 
+class SidebarCollapseUtil {
+  static #isCollapsed = false;
+
+  static init() {
+    this.#isCollapsed = localStorage.getItem(STORAGE_KEY) === 'true';
+    if (this.#isCollapsed) {
+      document.body.setAttribute(ATTR_COLLAPSED, '');
+    }
+  }
+
+  static toggle() {
+    this.#isCollapsed = !this.#isCollapsed;
+    document.body.toggleAttribute(ATTR_COLLAPSED, this.#isCollapsed);
+    localStorage.setItem(STORAGE_KEY, this.#isCollapsed);
+  }
+}
+
 export function initSidebar() {
   $trigger.onclick = $mask.onclick = () => SidebarUtil.toggle();
+
+  if ($collapseBtn) {
+    SidebarCollapseUtil.init();
+    $collapseBtn.onclick = () => SidebarCollapseUtil.toggle();
+  }
+
+  if ($expandBtn) {
+    $expandBtn.onclick = () => SidebarCollapseUtil.toggle();
+  }
 }
